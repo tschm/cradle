3504b70352d1bec1b4529ecbe1fb36088fb18b27
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-08 07:25:17 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# cvxcli
+
+[![PyPI version](https://badge.fury.io/py/cvxcli.svg)](https://badge.fury.io/py/cvxcli)
+[![Apache 2.0 License](https://img.shields.io/badge/License-APACHEv2-brightgreen.svg)](https://github.com/cvxgrp/cvxcli/blob/master/LICENSE)
+[![Downloads](https://static.pepy.tech/personalized-badge/cvxcli?period=month&units=international_system&left_color=black&right_color=orange&left_text=PyPI%20downloads%20per%20month)](https://pepy.tech/project/cvxcli)
+[![Coverage Status](https://coveralls.io/repos/github/cvxgrp/simulator/badge.png?branch=main)](https://coveralls.io/github/cvxgrp/cvxcli?branch=main)
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/cvxgrp/cvxcli)
+
+We demonstrate how to deploy to simple command line apps using uvx.
+
+## Install uvx
+
+The current way to deploy apps is to use [uvx](https://docs.astral.sh/uv/guides/tools/).
+The tool creates a temporary virtual environment for each app.
+
+For the installation of uv/uvx please refer to the [uvx documentation](https://docs.astral.sh/uv/getting-started/installation/).
+
+## Install cvxcli with uvx
+
+cvxcli has been deployed to PyPI like any other Python package. It could be
+installed via pip but we advise against that.
+
+cvxcli is not exactly a package you should use as a dependency in your own code.
+cvxcli is the home for two apps that are meant to be used from the command line.
+So please run via uvx
+
+```bash
+uvx --from cvxcli weather
+```
+
+This command will create a temporary virtual environment for cvxcli
+and install the package into that environment.
+
+Please note that you **do need uvx** to be installed on your machine.
---
+++ cvx/cli/weather.py
@@ -0,0 +1,51 @@
+#    Copyright 2023 Stanford University Convex Optimization Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+import fire  # type: ignore
+import requests  # type: ignore
+
+
+def cli(metric: str, latitude: float = 37.4419, longitude: float = -122.143) -> None:
+    """
+    Get the current weather for a given metric
+
+    Parameters
+    ----------
+    metric : str
+        The metric to get the current weather for.
+        Use time, temperature, windspeed, winddirection or weathercode
+        For details: https://open-meteo.com/en/docs
+    latitude : float, optional
+        The latitude to get the current weather for, by default 37.4419
+    longitude : float, optional
+        The longitude to get the current weather for, by default -122.143
+    """
+    url = "https://api.open-meteo.com/v1/forecast"
+    url = f"{url}?latitude={str(latitude)}&longitude={str(longitude)}&current_weather=true"
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        if metric in r.json()["current_weather"]:
+            x = r.json()["current_weather"][metric]
+            return x
+        else:
+            raise ValueError("Metric not supported!")
+    else:
+        raise ConnectionError("Open-Meteo is down!")
+
+
+def main():  # pragma: no cover
+    """
+    Run the CLI using Fire
+    """
+    fire.Fire(cli)
