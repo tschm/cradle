ac555f69675aacdf45a270d74871b8413925cc1d
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-09 22:56:10 +0400

    towards package

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,7 +21,7 @@ def test_paper(templates_dir, tmp_path):
         )
 
 
-def test_package(templates_dir, tmp_path):
+def test_pack(templates_dir, tmp_path):
     with pytest.raises(RuntimeError):
         cli(
             template=str(templates_dir / "package"),
