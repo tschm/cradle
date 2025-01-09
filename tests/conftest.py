4c50c395a3ea83263d2a5009f9a07d567fe5f0fb
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-09 13:01:20 +0400

    testing experiments

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,23 @@ import pytest
 def resource_fixture():
     """resource fixture"""
     return Path(__file__).parent / "resources"
+
+
+@pytest.fixture(scope="session", name="root")
+def root_fixture():
+    """resource fixture"""
+    return Path(__file__).parent.parent
+
+
+@pytest.fixture(scope="session", name="templates_dir")
+def templates_fixture(root):
+    """global fixtures for resources"""
+    return root / "cvx" / "cradle" / "templates"
+
+
+@pytest.fixture(scope="session", name="template")
+def template_fixture(templates_dir):
+    def f(name):
+        return str(templates_dir / name)
+
+    return f
---
+++ tests/test_experiments/test_template.py
@@ -0,0 +1,9 @@
+# from cvx.cradle.cli import cli
+#
+# from .utils import randomword
+#
+#
+# def test_build(templates_dir):
+#     # random name for the new repo
+#     name = randomword(10, prefix='')
+#     cli(template=str(templates_dir / "experiments"), user_defaults={"project_name": name})
