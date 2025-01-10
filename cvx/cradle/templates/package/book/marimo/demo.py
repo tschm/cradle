ee55a48f06cb28a3c139d112b65277f49f29b525
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-10 11:24:52 +0400

    towards a book

--- cvx/cradle/templates/package/book/marimo/demo.py
+++ cvx/cradle/templates/package/book/marimo/demo.py
@@ -11,3 +11,24 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+import marimo
+
+__generated_with = "0.10.10"
+app = marimo.App()
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Demo""")
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
