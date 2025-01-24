4cfab6548c5dd1a9b7cdd1cd190f3d831cb92a6a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-24 08:18:48 +0400

    move license to Thomas Schmelzer

--- src/cradle/utils/__init__.py
+++ src/cradle/utils/__init__.py
@@ -1,4 +1,4 @@
-#    Copyright 2025 Stanford University Convex Optimization Group
+#    Copyright 2025 Thomas Schmelzer
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
